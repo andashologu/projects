d9b284cec6f34c940b399c06633f12e260e1544b
Anda Shologu <SHOLOGUA@cput.ac.za>
2023-04-10 22:16:25 +0200

    added modal

--- propertycheck/src/main/resources/static/js/LoginModal.js
+++ propertycheck/src/main/resources/static/js/LoginModal.js
@@ -14,8 +14,18 @@ $("#login_form").on('submit', function(e){
         success: function(){
             location.reload();
         },
-        error: function(){
+        error: function(response){
             document.getElementById("error_label").innerHTML = "Incorrect username or password";
+            console.log("status: "+response.status);
+            console.log("statusText: "+response.statusText);
+            console.log("readyState: "+response.readyState);
+            console.log("responseText: "+response.responseText);
+            console.log("responseXML: "+response.responseXML);
+            console.log("responseJSON: "+response.responseJSON);
+            console.log("headerJSON: "+response.headerJSON);
+            console.log("request: "+response.request);
+            console.log("transport: "+response.transport);
+            
         }
     });
 });
